60ffceccc7c3df4404f2ae39e14dec5497591035
laputa-n <skatmdgur6365@gmail.com>
2025-08-29 15:49:09 +0900

    🌱 refactor: application.yml의 allowed-origins 문자열로 수정

--- apps/backend/src/main/java/com/calendarbox/backend/global/config/WebConfig.java
+++ apps/backend/src/main/java/com/calendarbox/backend/global/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.calendarbox.backend.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,7 +12,7 @@ import java.util.List;
 
 @Configuration
 public class WebConfig {
-    @Value("${security.cors.allowed-origins}")
+    @Value("#{'${security.cors.allowed-origins}'.split(',')}")
     private List<String> allowedOrigins;
 
     @Bean
