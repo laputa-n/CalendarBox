938bf9dcb23ffafaa27f5ba04a111989b5499475
laputa-n <skatmdgur6365@gmail.com>
2025-09-01 17:59:01 +0900

    🌱 feat: Friendship 엔티티 설계

---
+++ apps/backend/src/main/java/com/calendarbox/backend/friendship/repository/FriendshipRepository.java
@@ -0,0 +1,9 @@
+package com.calendarbox.backend.friendship.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class FriendshipRepository {
+
+}
