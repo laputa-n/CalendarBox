fd92d7c1067c8770953a65b7ec651ecdce6f81dc
laputa-n <skatmdgur6365@gmail.com>
2026-01-24 16:43:57 +0900

    🌱 refactor: Scheduler ON

--- apps/backend/src/main/java/com/calendarbox/backend/expense/support/OcrScheduler.java
+++ apps/backend/src/main/java/com/calendarbox/backend/expense/support/OcrScheduler.java
@@ -6,14 +6,13 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-//RabbitMQ 도입으로 비활성화
-//@Component
+@Component
 @RequiredArgsConstructor
 public class OcrScheduler {
     private final ExpenseOcrWorker expenseOcrWorker;
 
-    //@Scheduled(fixedDelay = 30000) // 5초 간격 추천
-    //@Async
+    @Scheduled(fixedDelay = 30000) // 10초 간격
+    @Async
     public void tick() {
         expenseOcrWorker.processBatch(10);
     }
